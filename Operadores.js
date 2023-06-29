2e4587286d8a6d69908c6b870761628c1235e8b2
Miguel Guzman <mguzmanck.2785@gmail.com>
2023-06-28 18:27:24 -0600

    Ejercicio demostrativo sobre operadores

---
+++ Operadores.js
@@ -0,0 +1,10 @@
+
+console.log(3+5);
+console.log(17-7);
+console.log(8*6);
+console.log(98%4);
+console.log(3+5*2-8);
+console.log("Hola"+ "Mike");
+var nombre = "Mike";
+var saludo = "Hola"+ nombre;
+console.log(saludo)
