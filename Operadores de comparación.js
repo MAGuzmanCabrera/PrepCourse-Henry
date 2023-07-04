0e94824f7ee6fbea8d80b43fef60997fac0cd517
Miguel Guzman <mguzmanck.2785@gmail.com>
2023-07-03 19:00:01 -0600

    Ejemplos de operadores de comparacion

---
+++ Operadores de comparación.js
@@ -0,0 +1,25 @@
+// Operaciones de comparacion
+console.log(4<7);
+console.log(8<9);
+console.log(4<4);
+console.log(15<7);
+console.log(4>7);
+console.log(9>7);
+
+
+//Igualdad vs igualdad estricta
+console.log(3==3);
+console.log(3===3);
+console.log(3=="3");
+console.log(3==="3");
+console.log(7=="7");
+console.log(7==="7");
+
+
+// Asignacion y asociatividad
+var a=1;
+var b=2;
+var c=a=b
+console.log(c);
+
+console.log(18/3/2);
